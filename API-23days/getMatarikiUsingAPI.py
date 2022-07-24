871ef1d307ab3ed1906eef203e12294d172a87d4
Sadat Taseen <sttaseen@gmail.com>
2022-07-24 14:31:25 +1200

    Change getClosestFriday to make Matariki be after NINETEENJUNE

--- API-23days/getMatarikiUsingAPI.py
+++ API-23days/getMatarikiUsingAPI.py
@@ -7,40 +7,42 @@ from scipy.signal import argrelextrema
 
 
 def getMatarikiFromAPI(year):
+    TWENTYTHREEDAYS = timedelta(days=23)
+    NINETEENJUNE = datetime(year, 6, 19)
+    # month_start_index = -1
 
     # API Platfprm used - https://web.postman.co/
-    url = fr"https://ssd.jpl.nasa.gov/api/horizons.api?format=json&COMMAND='301'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='{year}-05-26'&STOP_TIME='{year}-07-30'&STEP_SIZE='24h'&QUANTITIES='24"
+    url = fr"https://ssd.jpl.nasa.gov/api/horizons.api?format=json&COMMAND='301'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='{year}-05-26'&STOP_TIME='{year}-07-30'&STEP_SIZE='1h'&QUANTITIES='24"
 
     response = urllib.request.urlopen(url)
     result = json.loads(response.read())
     data = result['result'].partition('$$SOE')[2].partition('$$EOE')[0]
     times = data.split('\n')[1:-1]
-    stos = [times[i].split() for i in range(len(times))]
+    stos = np.array([times[i].split() for i in range(len(times))])
 
-    for time in stos:
-        del time[1]
+    stos_cleaned = stos[:, 2].astype(float)
+    new_moon = argrelextrema(stos_cleaned, np.greater)[0]
 
-    stos_cleaned = np.array(stos)[:, 1].astype(float)
-
-    new_moon = argrelextrema(stos_cleaned, np.greater)
-    TWENTYTHREEDAYS = timedelta(days=23)
-    NINETEENJUNE = datetime(year, 6, 19)
-
-    for new_month in new_moon[0]:
+    for i in range(len(new_moon)):
         tangaroa = datetime.strptime(
-            stos[new_month][0], '%Y-%b-%d') + TWENTYTHREEDAYS
+            stos[new_moon[i]][0], '%Y-%b-%d') + TWENTYTHREEDAYS
         if tangaroa >= NINETEENJUNE:
+            # month_start_index = i
             break
 
     def getClosestFriday(d0):
-        # If it's a Saturday, the previous Friday is chosen instead of the next
         if (d0.weekday() < 4):
-            return d0+timedelta(4-d0.weekday())
+            matariki = d0+timedelta(4-d0.weekday())
         else:
-            return d0-timedelta(d0.weekday()-4)
+            matariki = d0-timedelta(d0.weekday()-4)
+
+        if matariki < NINETEENJUNE:
+            matariki += timedelta(days=7)
+
+        return matariki
 
     # print(f"date : {date}")
     return getClosestFriday(tangaroa).strftime('%Y-%m-%d')
 
 
-print(getMatarikiFromAPI(2022))
+# print(getMatarikiFromAPI(2027))

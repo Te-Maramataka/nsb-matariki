623d20c1362df62b50546cacd8421fa49fa2d395
Sadat Taseen <sttaseen@gmail.com>
2022-07-24 21:23:26 +1200

    Change constants: TWENTYTHREEDAYS and NINETEENJUNE

--- API-23days/getMatarikiUsingAPI.py
+++ API-23days/getMatarikiUsingAPI.py
@@ -1,34 +1,25 @@
 import numpy as np
 import json
 import urllib.request
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 from scipy.signal import argrelextrema
 
 
 def getMatarikiFromAPI(year):
-    TWENTYTHREEDAYS = timedelta(days=23)
-    NINETEENJUNE = datetime(year, 6, 19)
-    # month_start_index = -1
+    TWENTYTHREEDAYS = timedelta(days=22)
+    NINETEENJUNE = datetime(year, 6, 18)
 
-    # API Platfprm used - https://web.postman.co/
-    url = fr"https://ssd.jpl.nasa.gov/api/horizons.api?format=json&COMMAND='301'&OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&CENTER='500@399'&START_TIME='{year}-05-26'&STOP_TIME='{year}-07-30'&STEP_SIZE='1h'&QUANTITIES='24"
-
+    url = fr"https://ssd.jpl.nasa.gov/api/horizons.api?OBJ_DATA='YES'&MAKE_EPHEM='YES'&EPHEM_TYPE='OBSERVER'&format=json&COMMAND='301'&QUANTITIES='24'&CENTER='500@399'&START_TIME='{year}-05-26'&STOP_TIME='{year}-07-30'&TIME_ZONE='+12:00'&STEP_SIZE='1h'"
     response = urllib.request.urlopen(url)
     result = json.loads(response.read())
     data = result['result'].partition('$$SOE')[2].partition('$$EOE')[0]
     times = data.split('\n')[1:-1]
-    stos = np.array([times[i].split() for i in range(len(times))])
-
+    stos = [times[i].split() for i in range(len(times))]
+    stos = np.array(stos)
     stos_cleaned = stos[:, 2].astype(float)
     new_moon = argrelextrema(stos_cleaned, np.greater)[0]
 
-    for i in range(len(new_moon)):
-        tangaroa = datetime.strptime(
-            stos[new_moon[i]][0], '%Y-%b-%d') + TWENTYTHREEDAYS
-        if tangaroa >= NINETEENJUNE:
-            # month_start_index = i
-            break
+    stos = stos[:-1]
 
     def getClosestFriday(d0):
         if (d0.weekday() < 4):
@@ -41,8 +32,13 @@ def getMatarikiFromAPI(year):
 
         return matariki
 
-    # print(f"date : {date}")
+    for i in range(len(new_moon)):
+        tangaroa = datetime.strptime(
+            stos[new_moon[i]][0], '%Y-%b-%d') + TWENTYTHREEDAYS
+        if tangaroa >= NINETEENJUNE:
+            break
+
     return getClosestFriday(tangaroa).strftime('%Y-%m-%d')
 
 
-# print(getMatarikiFromAPI(2027))
+print(getMatarikiFromAPI(2025))
